5b313b57025f846db1c10274d679e482f9f880d4
Ivonilson <ivonilsonjcardoso@gmail.com>
2025-12-17 08:57:05 -0300

    update

--- app.py
+++ app.py
@@ -204,4 +204,4 @@ def cleanup():
                          ghostscript_installed=bool(get_ghostscript_command()))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0')
